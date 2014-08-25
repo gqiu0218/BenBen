6548b91594133af8f73320b4ebff079b9592c34b
wanmeiqg <1076757268@qq.com>
2014-08-25 21:23:42 +0800

    字符串工具类

---
+++ src/com/benben/utils/StringUtils.java
@@ -0,0 +1,16 @@
+package com.benben.utils;
+
+public class StringUtils
+{
+
+    /**
+     * ÅÐ¶Ï×Ö·û´®ÊÇ·ñÎª¿Õ
+     * 
+     * @param sourse
+     * @return
+     */
+    public static boolean isNotNull(String sourse)
+    {
+        return sourse != null && !sourse.equals("");
+    }
+}
